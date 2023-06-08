78bf96cd956fbedfa4c3f007f93bba9ca0ffde7c
konradkar2 <konradkar2@gmail.com>
2023-06-08 17:36:29 +0200

    fix uart header

--- include/uart.h
+++ include/uart.h
@@ -11,7 +11,7 @@ int uart_getchar(FILE *stream);
 
 void uart_init(void);
 
-FILE uart_output = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
-FILE uart_input = FDEV_SETUP_STREAM(NULL, uart_getchar, _FDEV_SETUP_READ);
+FILE open_uart_ouput();
+FILE open_uart_input();
 
 #endif
