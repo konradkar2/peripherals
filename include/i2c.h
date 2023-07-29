013deedfaa40f50dac0bdc65b31ffebadeb5c156
K.Karas <konradkar2@gmail.com>
2023-07-29 17:40:37 +0200

    implement i2c driver (in progress)

--- include/i2c.h
+++ include/i2c.h
@@ -1,21 +1,21 @@
 #ifndef I2C_H
 #define I2C_H
 
-#include <stdint.h>
-#include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/io.h>
 #include <pin.h>
+#include <stddef.h>
+#include <stdint.h>
 
-struct i2c_driver
-{
-    uint8_t address;
+struct i2c_driver {
+	uint8_t address;
 };
 typedef struct i2c_driver i2c_driver;
 
 void i2c_driver_init(i2c_driver *drv, uint8_t address);
-int i2c_driver_start(i2c_driver *drv);
-void i2c_driver_stop(i2c_driver *drv);
-int i2c_driver_transmit(i2c_driver * drv, uint8_t cData);
-uint8_t spi_driver_read();
+int8_t i2c_driver_start(i2c_driver *drv);
+int8_t i2c_driver_stop();
+int8_t i2c_driver_transmit(uint8_t cData);
+int8_t i2c_driver_read(i2c_driver *drv, size_t n, uint8_t *out);
 
 #endif
--- src/i2c.c
+++ src/i2c.c
@@ -1,9 +1,27 @@
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include <i2c.h>
+#include <stdio.h>
 #include <util/twi.h>
 
 #define LOG() printf("%s\n", __func__)
+
+#define WAIT_FOR_TWI()                                                         \
+	while (!(TWCR & (1 << TWINT))) {                                       \
+	}
+
+#define TW_STATUS_ASSERT(EXPECTED_TW_STATUS)                                   \
+	{                                                                      \
+		const uint8_t tw_status = TW_STATUS;                           \
+		if (EXPECTED_TW_STATUS != tw_status) {                         \
+			printf("ERR: %s (%d): tw status NOK: actual 0x%02x, "  \
+			       "expected: "                                    \
+			       "0x%02x (%s) \n",                               \
+			       __func__, __LINE__, tw_status,                  \
+			       EXPECTED_TW_STATUS, #EXPECTED_TW_STATUS);       \
+			return tw_status;                                      \
+		}                                                              \
+	}
 
 void i2c_driver_init(i2c_driver *drv, uint8_t address)
 {
@@ -11,57 +29,98 @@ void i2c_driver_init(i2c_driver *drv, uint8_t address)
 	drv->address = address;
 }
 
-int i2c_driver_start(i2c_driver *drv)
+int8_t i2c_driver_start(i2c_driver *drv)
 {
 	LOG();
 	// Clear TW interrupt flag, Set TW start, set TW enable
 	TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
 
-	// wait for completion
 	LOG();
-	while (!(TWCR & (1 << TWINT))) {
-	}
+	WAIT_FOR_TWI();
+	TW_STATUS_ASSERT(TW_START);
 
 	// load slave address, and wait for ACK
-	TWDR = drv->address;
+	// set last bit to 0 (write)
+	TWDR = drv->address & 0xFE;
 	// transmit slave address
 	TWCR = (1 << TWINT) | (1 << TWEN);
 
-	// wait for ACK/NACK
+	// wait for slave ACK/NACK
 	LOG();
-	while (!(TWCR & (1 << TWINT))) {
-	}
+	WAIT_FOR_TWI();
+	TW_STATUS_ASSERT(TW_MT_SLA_ACK);
 
-	LOG();
-	if ((TWSR & 0xF8) != TW_MT_SLA_ACK) {
-		return 1;
-	}
+	return 0;
 }
 
-void i2c_driver_stop(i2c_driver *drv)
+int8_t i2c_driver_stop()
 {
-	(void)drv;
 	LOG();
 	TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO);
+	WAIT_FOR_TWI();
+	TW_STATUS_ASSERT(TW_SR_STOP);
+
+	return 0;
 }
 
-int i2c_driver_transmit(i2c_driver *drv, uint8_t data)
+int8_t i2c_driver_transmit(uint8_t data)
 {
-	(void)drv;
 	LOG();
+	if (!(TWCR & (1 << TWINT))) {
+		return 1;
+	}
 
 	TWDR = data;
 	TWCR = (1 << TWINT) | (1 << TWEN);
 
 	LOG();
 	// wait for ACK/NACK
+	WAIT_FOR_TWI();
+	TW_STATUS_ASSERT(TW_MT_DATA_ACK)
+	return 0;
+}
+
+int8_t i2c_driver_read(i2c_driver *drv, const size_t n, uint8_t *out)
+{
+	LOG();
+	// Clear TW interrupt flag, Set TW start, set TW enable
+	TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
+
+	LOG();
+	// wait for completion
+	WAIT_FOR_TWI();
+	TW_STATUS_ASSERT(TW_START)
+
+	// load slave address, and wait for ACK
+	// set last bit to 1 (read)
+	TWDR = drv->address | 0x01;
+
+	// transfer
+	TWCR = (1 << TWINT) | (1 << TWEN);
+
+	LOG();
+	// wait for response
 	while (!(TWCR & (1 << TWINT))) {
 	}
 
-	LOG();
-	if ((TWSR & 0xF8) != TW_MT_DATA_ACK) {
-		return 1;
+	// check for master read ACK
+	TW_STATUS_ASSERT(TW_MR_SLA_ACK)
+
+	// read n-1 bytes
+	for (size_t i = 0; i < n - 1; ++i) {
+		// receive data and wait for ACK
+		TWCR = (1 << TWINT) | (1 << TWEA);
+		// wait for data
+		WAIT_FOR_TWI();
+		TW_STATUS_ASSERT(TW_MR_DATA_ACK)
+		*out = TWDR;
+		++out;
 	}
+
+	// read last byte and transmit NACK
+	TWCR = (1 << TWINT);
+	*out = TWDR;
+
+	TW_STATUS_ASSERT(TW_MR_DATA_NACK)
 	return 0;
 }
-uint8_t spi_driver_read() { return 0; }
